0b7b10a2a7128ddffae2c2abadd7364d7a0f9d4f
Miguel Arturo Pujol Ochoa <miguelarturopujolochoa@Woloxs-MacBook-Pro-9.local>
2018-09-03 17:32:11 -0500

    promises

--- react/fingerex/02 - promises/index.js
+++ react/fingerex/02 - promises/index.js
@@ -1,9 +1,15 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(a) {
+  const sign = Math.random() < 0.5 ? -1 : 1;
+  const d = new Promise((resolve, reject) => {
+    if (a < 2 * a) resolve(a + (sign * Math.floor(Math.random() * 101)));
+    else reject(new Error('This time is too much !'));
+  });
+  return d;
 }
 
-export function asyncDelay() {
-
+export function asyncDelay(b) {
+  const sign = Math.random() < 0.5 ? -1 : 1;
+  return (b + (sign * Math.floor(Math.random() * 101)));
 }
