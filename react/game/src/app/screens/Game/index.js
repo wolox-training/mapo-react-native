602cb286ccbad96255b255fe42885f26d92c3694
Miguel Arturo Pujol Ochoa <miguelarturopujolochoa@Woloxs-MacBook-Pro-9.local>
2018-09-05 15:28:38 -0500

    Showing the Past Moves

--- react/game/src/app/screens/Game/index.js
+++ react/game/src/app/screens/Game/index.js
@@ -35,6 +35,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -43,26 +54,16 @@ class Game extends React.Component {
     }
 
     return (
-<<<<<<< HEAD
       <div className={styles.game}>
         <div className={styles.gameBoard}>
-          <Board />
-        </div>
-        <div className={styles.gameInfo}>
-          <div>{/* status */}</div>
-=======
-      <div className="game">
-        <div className="game-board">
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
-
         </div>
         <div className="game-info">
           <div>{status}</div>
->>>>>>> Lifting State Up, Again
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
