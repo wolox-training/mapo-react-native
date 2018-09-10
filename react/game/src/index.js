db5c057c2a07113b2833838dc148312e73e26e41
Miguel Arturo Pujol Ochoa <miguelarturopujolochoa@Woloxs-MacBook-Pro-40.local>
2018-09-10 17:43:18 -0500

    redirect router

--- react/game/src/index.js
+++ react/game/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import './scss/index.scss';
 import Login from './app/screens/Login';
+import Game from './app/screens/Game';
 
 const reducers = {
   form: formReducer
@@ -14,9 +17,23 @@ const reducers = {
 const reducer = combineReducers(reducers);
 const store = (window.devToolsExtension ? window.devToolsExtension()(createStore) : createStore)(reducer);
 
+const loggedin = true;
+const App = props => (
+  <Router basename={props.path}>
+    <div>
+      <Route exact path="/" render={() => <Login />} />
+      <Route path="/game" render={() => (loggedin ? <Game /> : <Redirect to="/" />)} />
+    </div>
+  </Router>
+);
+
+App.propTypes = {
+  path: PropTypes.string
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <Login />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
