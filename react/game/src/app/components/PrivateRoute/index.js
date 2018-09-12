2a89eba26576d954ceec03b5f0738c4b53129b76
Miguel Arturo Pujol Ochoa <miguelarturopujolochoa@Woloxs-MacBook-Pro-40.local>
2018-09-12 14:57:13 -0500

    private route to class

--- react/game/src/app/components/PrivateRoute/index.js
+++ react/game/src/app/components/PrivateRoute/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const PrivateRoute = ({ component: Component, loggedin, ...rest }) => {
-  const res = props => (loggedin ? <Component {...props} /> : <Redirect to="/" />);
+class PrivateRoute extends Component {
+  res = () => {
+    const AuthComponent = this.props.component;
+    return this.props.loggedin ? <AuthComponent path={this.props.path} /> : <Redirect to="/" />;
+  };
 
-  return <Route {...rest} render={res} />;
+  render() {
+    return <Route render={this.res} />;
+  }
+}
+
+PrivateRoute.propTypes = {
+  path: PropTypes.string.isRequired,
+  component: PropTypes.func.isRequired,
+  loggedin: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
