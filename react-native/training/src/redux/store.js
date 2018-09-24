0b277c4808144b2be69d9a4075653c326ed5e6e7
Wally <walterfolini@gmail.com>
2018-09-24 10:08:57 -0500

    React native kickoff

---
+++ react-native/training/src/redux/store.js
@@ -0,0 +1,41 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import Reactotron from 'reactotron-react-native';
+import thunk from 'redux-thunk';
+import { 
+  createReactNavigationReduxMiddleware,
+  createNavigationReducer
+} from 'react-navigation-redux-helpers';
+import { fetchMiddleware } from 'redux-recompose';
+
+import { ROOT } from '../constants/platform';
+import Navigator from '../app/screens';
+
+import auth from './auth/reducer';
+
+const nav = createNavigationReducer(Navigator);
+
+const reducers = combineReducers({
+  auth,
+  nav
+});
+
+const middlewares = [];
+const enhancers = [];
+
+/* ------------- React Navigation Middleware ------------- */
+middlewares.push(createReactNavigationReduxMiddleware(ROOT, state => state.nav));
+
+/* ------------- Thunk Middleware ------------- */
+middlewares.push(thunk);
+
+/* ------------- Redux-Recompose Middleware ------------- */
+middlewares.push(fetchMiddleware);
+
+/* ------------- Assemble Middleware ------------- */
+enhancers.push(applyMiddleware(...middlewares));
+
+// in dev mode, we'll create the store through Reactotron
+const createAppropriateStore = __DEV__ ? Reactotron.createStore : createStore;
+const store = createAppropriateStore(reducers, compose(...enhancers));
+
+export default store;
