803bc056c7a9a0c96e7aa2873c5ed9721a8bd600
Miguel Arturo Pujol Ochoa <miguelarturopujolochoa@Woloxs-MacBook-Pro-40.local>
2018-09-28 14:08:56 -0500

    rebase fixes

--- react-native/training/src/app/screens/Dummy/components/List/components/RenderItem/index.js
+++ react-native/training/src/app/screens/Dummy/components/List/components/RenderItem/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import PropTypes from 'prop-types';
 
 import booksTypes from '../../../../../../../types/booksTypes';
 import styles from '../../styles';
 
 import Picture from './components/Picture';
 
-const RenderItem = ({ item }) => (
-  <View style={styles.item} key={item.id}>
+const RenderItem = ({ item, navigation }) => (
+  <TouchableOpacity
+    style={styles.item}
+    key={item.id}
+    onPress={() => {
+      navigation.navigate('BookDetails', {
+        item
+      });
+    }}
+  >
     <Picture imgUrl={item.image_url} />
     <View>
       <Text style={styles.title}>{item.title}</Text>
       <Text style={styles.author}>{item.author}</Text>
     </View>
-  </View>
+  </TouchableOpacity>
 );
 
 RenderItem.propTypes = {
-  item: booksTypes.book
+  item: booksTypes.book,
+  navigation: PropTypes.shape({ navigate: PropTypes.func }).isRequired
 };
 
-export default RenderItem;
+export default withNavigation(RenderItem);
